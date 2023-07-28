11f4dc0f489a50ad01f608a686a596aead54a8a9
madyanes <madyanes.dev@gmail.com>
2023-07-28 09:30:12 +0700

    limit title input to max 50 chars

--- src/components/NewNoteInput.jsx
+++ src/components/NewNoteInput.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 class NewNote extends React.Component {
+    state = {
+        text: ''
+    }
+
+    onChangeInput = (e) => {
+        const inputText = e.target.value
+        if (inputText.length <= 50) {
+            this.setState({ text: inputText })
+        }
+    }
+
     render() {
         return (
             <section className='new-note'>
                 <h1>Catatan Baru</h1>
                 <form action="#">
-                    <input type="text" placeholder='Judul catatan' />
+                    <div className="character-count">
+                        <p>Sisa Karakter: {50 - this.state.text.length}</p>
+                    </div>
+                    <input type="text"
+                        placeholder='Judul catatan'
+                        maxLength={50}
+                        onChange={this.onChangeInput}
+                    />
                     <textarea cols="30" rows="10" placeholder='Isi catatan'></textarea>
                     <button>Buat</button>
                 </form>
