c5f8a0b9acaa33b53476a99c24d35c1f3b97490b
madyanes <madyanes.dev@gmail.com>
2023-07-28 07:45:44 +0700

    show empty message if no active notes Jika menggunakan functional component harus menggunakan useEffect, jika menggunakan class Component tidak perlu. Sumber: ChatGPT.

--- src/components/ActiveNotes.jsx
+++ src/components/ActiveNotes.jsx
@@ -1,27 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ActiveNoteItems from './ActiveNoteItems'
 
-const ActiveNotes = ({ notes, showFormattedDate, onDelete, onArchive }) => {
-    return (
-        <section className='notes'>
-            <h1>Active Notes</h1>
-            <div className="active-notes">
-                {
-                    notes
-                        .filter((note) => note.archived === false)
-                        .map((note) => (
-                            <ActiveNoteItems
-                                key={note.id}
-                                note={note}
-                                showFormattedDate={showFormattedDate}
-                                onDelete={onDelete}
-                                onArchive={onArchive}
-                            />
-                        ))
-                }
-            </div>
-        </section>
-    )
+class ActiveNotes extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            activeNotes: []
+        }
+    }
+
+    componentDidMount() {
+        this.filterActiveNotes()
+    }
+
+    componentDidUpdate(prevProps) {
+        if (prevProps.notes !== this.props.notes) {
+            this.filterActiveNotes()
+        }
+    }
+
+    filterActiveNotes = () => {
+        const { notes } = this.props
+        const activeNotes = notes.filter((note) => !note.archived)
+        this.setState({ activeNotes })
+    }
+
+    render() {
+        const { showFormattedDate, onDelete, onArchive } = this.props
+        const { activeNotes } = this.state
+
+        return (
+            <section className='notes'>
+                <h1>Active Notes</h1>
+                <div className="active-notes">
+                    {
+                        activeNotes.length ? (
+                            activeNotes.map((note) => (
+                                <ActiveNoteItems
+                                    key={note.id}
+                                    note={note}
+                                    showFormattedDate={showFormattedDate}
+                                    onDelete={onDelete}
+                                    onArchive={onArchive}
+                                />
+                            ))
+                        ) : (
+                            <p>Tidak ada catatan.</p>
+                        )
+                    }
+                </div>
+            </section>
+        )
+    }
 }
 
 export default ActiveNotes
