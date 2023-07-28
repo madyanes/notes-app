1c97dc0315578826f0ca87e88c900c8c6aad8128
madyanes <madyanes.dev@gmail.com>
2023-07-28 07:59:50 +0700

    show empty message if no archived notes

--- src/components/ArchivedNotes.jsx
+++ src/components/ArchivedNotes.jsx
@@ -1,27 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ArchivedNoteItems from './ArchivedNoteItems'
 
-const ArchivedNotes = ({ notes, showFormattedDate, onDelete, onArchive }) => {
-    return (
-        <section className='notes'>
-            <h1>Archived Notes</h1>
-            <div className="active-notes">
-                {
-                    notes
-                        .filter((note) => note.archived === true)
-                        .map((note) => (
-                            <ArchivedNoteItems
-                                key={note.id}
-                                note={note}
-                                showFormattedDate={showFormattedDate}
-                                onDelete={onDelete}
-                                onArchive={onArchive}
-                            />
-                        ))
-                }
-            </div>
-        </section>
-    )
+class ArchivedNotes extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            archivedNotes: []
+        }
+    }
+
+    componentDidMount() {
+        this.filterArchivedNotes()
+    }
+
+    filterArchivedNotes = () => {
+        const { notes } = this.props
+        const archivedNotes = notes.filter((note) => note.archived)
+        this.setState({ archivedNotes })
+    }
+
+    render() {
+        const { notes, showFormattedDate, onDelete, onArchive } = this.props;
+        const archivedNotes = notes.filter((note) => note.archived === true);
+
+        return (
+            <section className='notes'>
+                <h1>Archived Notes</h1>
+                <div className="active-notes">
+                    {
+                        archivedNotes.length ? (
+                            archivedNotes.map((note) => (
+                                <ArchivedNoteItems
+                                    key={note.id}
+                                    note={note}
+                                    showFormattedDate={showFormattedDate}
+                                    onDelete={onDelete}
+                                    onArchive={onArchive}
+                                />
+                            ))
+                        ) : (
+                            <p>Tidak ada catatan.</p>
+                        )
+                    }
+                </div>
+            </section>
+        );
+    }
 }
 
 export default ArchivedNotes
